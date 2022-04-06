10dbd719523bdcb51077448b04cf28f3c51f15a3
darkTi <892184375@qq.com>
2022-04-06 22:45:44 +0800

    1、使用express generator脚手架；2、使用ejs模板；3、改用ts

---
+++ routes/index.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from 'express';
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'WINWIN' });
+} as RequestHandler);
+
+module.exports = router;
